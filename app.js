57c44a12a4e3e0190b59bc390604f03da82a1da1
ahmedraza <ahmed.raza1054@gmail.com>
2023-12-15 04:23:59 +0500

    todo app ready

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ let list_item = document.getElementById("list_item");
 const addItem = (e) => {
   e.preventDefault();
   if (input.value.trim() !== "") {
-    if (count < 6 && input.value.length < 22) {
+    if (count < 6 && input.value.length < 20) {
       list_item.innerHTML += `
         <div class="list_item">
           <div>
             <p>${count} - </p>
-            <p>${input.value}</p>
+            <p class="para">${input.value}</p>
           </div>
           <div>
-            <button id="edit_btn">Edit</button>
-            <button id="del_btn">Delete</button>
+            <button class="edit_btn">Edit</button>
+            <button class="del_btn">Delete</button>
           </div>
         </div>
       `;
       count++;
-      input.value = "";
-      addEventListeners(); // Attach event listeners to new buttons
+      addEventListeners();
     } else {
       alert("Reached the count");
     }
   } else {
     alert("Can't add an empty value");
   }
+  input.value = "";
 };
 
 const addEventListeners = () => {
-  let editButtons = document.getElementById("#edit_btn");
-  let deleteButtons = document.getElementById("#del_btn");
+  let editButtons = document.querySelectorAll(".edit_btn");
+  let deleteButtons = document.querySelectorAll(".del_btn");
 
   editButtons.forEach((button, i) => {
     button.addEventListener("click", () => {
@@ -49,8 +49,14 @@ const addEventListeners = () => {
 const editItem = (index) => {
   let newValue = prompt("Enter new value:");
   if (newValue !== null) {
-    let paragraphs = document.querySelectorAll(".list_item p:nth-child(2)");
-    paragraphs[index].textContent = newValue;
+    if (newValue.length < 20) {
+      let paragraphs = document.querySelectorAll(".para");
+      paragraphs[index].textContent = newValue;
+    } else {
+      alert("value should be lower 22 leter");
+    }
+  } else {
+    alert("Can't add an empty value");
   }
 };
 
